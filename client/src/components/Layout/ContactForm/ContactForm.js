4f07a262cd5d5900d060c198bfd1a91347a7707a
Esteban Quintana Cueto <estebanquintana6@gmail.com>
2021-07-20 15:32:57 -0500

    Contact form image changed

--- client/src/components/Layout/ContactForm/ContactForm.js
+++ client/src/components/Layout/ContactForm/ContactForm.js
@@ -4,45 +4,45 @@ import { Row } from 'react-bootstrap'
 
 const ContactForm = () => {
     return (
-        <div className="book">
-            <div className="book__form">
-                <form action="#" className="form">
-                    <div className="u-margin-bottom-medium">
-                        <h2 className="heading-secondary">
-                            Escala con nostros!
+        <div class="book">
+            <div class="book__form">
+                <form action="#" class="form">
+                    <div class="u-margin-bottom-medium">
+                        <h2 class="heading-secondary">
+                            Escala con nosotros
                         </h2>
                     </div>
 
-                    <div className="form__group">
-                        <input type="text" className="form__input" placeholder="Tu nombre" id="name" required />
-                        <label className="form__label">Nombre</label>
+                    <div class="form__group">
+                        <input type="text" class="form__input" placeholder="Nombre" id="name" required />
+                        <label for="name" class="form__label">Nombre</label>
                     </div>
 
-                    <div className="form__group">
-                        <input type="email" className="form__input" placeholder="Tu email" id="email" required />
-                        <label className="form__label">Email</label>
+                    <div class="form__group">
+                        <input type="email" class="form__input" placeholder="Email" id="email" required />
+                        <label for="email" class="form__label">Email</label>
                     </div>
 
-                    <div className="form__group u-margin-bottom-medium">
-                        <div className="form__radio-group">
-                            <input type="radio" className="form__radio-input" id="small" name="size" />
-                            <label className="form__radio-label">
-                                <span className="form__radio-button"></span>
-                                Solo yo
+                    <div class="form__group u-margin-bottom-medium">
+                        <div class="form__radio-group">
+                            <input type="radio" class="form__radio-input" id="small" name="size" />
+                            <label for="small" class="form__radio-label">
+                                <span class="form__radio-button"></span>
+                                Yo solo
                             </label>
                         </div>
 
-                        <div className="form__radio-group">
-                            <input type="radio" className="form__radio-input" id="large" name="size" />
-                            <label className="form__radio-label">
-                                <span className="form__radio-button"></span>
+                        <div class="form__radio-group">
+                            <input type="radio" class="form__radio-input" id="large" name="size" />
+                            <label for="large" class="form__radio-label">
+                                <span class="form__radio-button"></span>
                                 En grupo
                             </label>
                         </div>
                     </div>
 
-                    <div className="form__group">
-                        <button className="btn btn--green">Next step &rarr;</button>
+                    <div class="form__group">
+                        <button class="btn btn--green">Next step &rarr;</button>
                     </div>
                 </form>
             </div>
